bb5e5f31e626dfdd8339136b357d2a657d8918ff
IgnaxA <ignat.panoff@gmail.com>
2024-01-12 01:48:20 +0500

    fixed wrong repository name

--- src/main/java/com/ip/bankmanager/bankmanager/Repositories/ClientRepository.java
+++ src/main/java/com/ip/bankmanager/bankmanager/Repositories/ClientRepository.java
@@ -3,6 +3,6 @@ package com.ip.bankmanager.bankmanager.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ip.bankmanager.bankmanager.Entities.Client;
 
-public interface ClientInterface extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Integer> {
     
 }
