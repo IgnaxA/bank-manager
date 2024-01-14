ba682bbdf86789671a98929bb55f82d98e7e1701
IgnaxA <ignat.panoff@gmail.com>
2024-01-14 23:48:05 +0500

    added new exceptions throws bc of missing update realisation in jpa

--- src/main/java/com/ip/bankmanager/interfaces/BaseEntityInterface.java
+++ src/main/java/com/ip/bankmanager/interfaces/BaseEntityInterface.java
@@ -1,11 +1,12 @@
 package com.ip.bankmanager.interfaces;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public interface BaseEntityInterface<T> {
     List<T> getAll();
     T getEntity(int id);
     T createEntity(T entity);
-    T updateEntity(int id, T newEntity);
+    T updateEntity(int id, T newEntity) throws IllegalAccessException, InvocationTargetException;
     void deleteEntity(int id);
 }
